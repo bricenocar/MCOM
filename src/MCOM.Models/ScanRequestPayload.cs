29c22e5607f13a8d74e90edb18048b288ee74250
JUBRI <jubri@equinor.com>
2022-02-23 08:57:01 +0100

    Adding scan on demand function code

---
+++ src/MCOM.Models/ScanRequestPayload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MCOM.Models
+{
+    public class ScanRequestPayload
+    {
+        public string Id { get; set; }
+        public string WBS { get; set; }
+        public string RequestedBy { get; set; }
+        public DateTime RequestedDate { get; set; }
+        public string DocumentName { get; set; }
+        public string Vendor { get; set; }
+        public string Comments { get; set; }
+        public Guid OrderNumber { get; set; }
+        public string Status { get; set; }
+        public bool IsPhysical { get; set; }
+    }
+}
