0ec3dc6f6bb75882de5cb760345ba53eab06124e
JUBRI <jubri@equinor.com>
2022-06-21 12:50:11 +0200

    Adding new fields to EF

--- src/MCOM.Models/EntityFramework/Governance/McomscanExecution.cs
+++ src/MCOM.Models/EntityFramework/Governance/McomscanExecution.cs
@@ -8,6 +8,12 @@ namespace MCOM.Models.EntityFramework.Governance
         public DateTime? Datescanned { get; set; }
         public Guid? RequestId { get; set; }
         public int? Size { get; set; }
+        public string? Documentdate { get; set; }
+        public string? Author { get; set; }
+        public string? License { get; set; }
+        public string? Field { get; set; }
+        public string? Well { get; set; }
+        public string? Installation { get; set; }
 
         public virtual McomscanRequest? Request { get; set; }
     }
