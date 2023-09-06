4ed3270fc2250757edb958575ff993cc3549d79d
Juan Briceno <juan.briceno@hotmail.com>
2023-09-06 09:53:10 +0200

    temp saving with errors

--- src/MCOM.Provisioning.Functions/GetOptionalValues.cs
+++ src/MCOM.Provisioning.Functions/GetOptionalValues.cs
@@ -1,30 +1,64 @@
 using System.Net;
+using MCOM.Models;
+using MCOM.Utilities;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace MCOM.Provisioning.Functions
 {
     public class GetOptionalValues
-    {
-        private readonly ILogger _logger;
-
-        public GetOptionalValues(ILoggerFactory loggerFactory)
+    {      
+        public GetOptionalValues()
         {
-            _logger = loggerFactory.CreateLogger<GetOptionalValues>();
+           
         }
 
         [Function("GetOptionalValues")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, FunctionContext context)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            var logger = context.GetLogger("ValidateTemplate");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            try
+            {
+                GlobalEnvironment.SetEnvironmentVariables(logger);
+            }
+            catch (Exception ex)
+            {
+                var msg = "Config values missing or bad formatted in app config.";
+                Global.Log.LogError(ex, msg + "Error: {ErrorMessage}", ex.Message);
+                return HttpUtilities.HttpResponse(req, HttpStatusCode.InternalServerError, "false");
+            }
 
-            response.WriteString("Welcome to Azure Functions!");
+            try
+            {
+                // Temporary static code to build the templates
+                var availableTemplates = new List<GetAvailableTemplatesPayload>()
+                {
+                    new GetAvailableTemplatesPayload()
+                    {
+                        FileName = "Json Template",
+                        FilePath = "templates/JsonFile.json"
+                    },
+                    new GetAvailableTemplatesPayload()
+                    {
+                        FileName = "Xml Template",
+                        FilePath = "templates/XmlFile.xml"
+                    }
+                };
 
-            return response;
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "application/json");
+                response.WriteString(JsonConvert.SerializeObject(availableTemplates));
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Global.Log.LogError(ex, "Error: {ErrorMessage}", ex.Message);
+                return HttpUtilities.HttpResponse(req, HttpStatusCode.InternalServerError, "false");
+            }
         }
     }
 }
